a1bcf930b5d225adcc776543a92f107ef85eab0b
sanghoya3 <sanghoya3@naver.com>
2023-05-17 15:00:55 +0900

    [feat] adding layout

---
+++ triends/src/components/layout/PageDetailHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="detail-header">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: "PageDetailHeader",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<style>
+.detail-header {
+  text-align: left;
+  font-size: 3rem;
+  font-weight: bold;
+  background-color: pink;
+}
+</style>
