00c3241a1151102a4c38a994645ca0e4d7406226
Sehbeom <j3beom@gmail.com>
2023-05-23 04:45:41 +0900

    feat(notification): 알림 목록 추가

--- triends/src/components/layout/HeaderNaviBar.vue
+++ triends/src/components/layout/HeaderNaviBar.vue
@@ -10,6 +10,39 @@
         />
       </router-link>
       <div class="header-right">
+        <b-dropdown class="p-0 m-0" size="lg" variant="link" dropright toggle-class="text-decoration-none" no-caret>
+          <template #button-content>
+            <div 
+              :class="notificationClicked ? 'notification' : 'notification alarmanimation'"
+              @click="notificationClick">
+                <div class="reddot" v-if="notifications.length"></div>
+                <img 
+                  src="../../assets/icons/notification.png"
+                  width="40"
+                  height="40" />
+            </div>
+          </template>
+          <div class="notification-dropdown">
+              <div 
+                class="notification-dropdown-onenoti" 
+                v-for="(notification, index) in notifications"
+                :key="notification.notificationId">
+                <div class="notification-dropdown-onenoti-contents">
+                  <div class="notification-dropdown-text">
+                    <span>{{ notification.content }}</span>
+                  </div>
+                  <div class="notification-dropdown-btns">
+                    <b-button size="sm" variant="success">수락</b-button>
+                    <b-button size="sm" variant="danger">거절</b-button>
+                  </div>
+                </div>
+                <div class="notification-dropdown-divider" v-if="index < (notifications.length - 1)"></div>
+              </div>
+              
+          </div>
+          
+        </b-dropdown>
+
         <img
           src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People/Bust%20in%20Silhouette.png"
           alt="Bust in Silhouette"
@@ -39,6 +72,52 @@ export default {
   components: {
     UserLoginModal,
   },
+  created() {
+    this.notifications = [
+      {
+        notificationId: 1,
+        content: "전상호섹시가이님이 친구 요청을 보냈습니다."
+      },
+      {
+        notificationId: 2,
+        content: "신우종폼미쳐타이님이 친구 요청을 보냈습니다."
+      },
+      {
+        notificationId: 3,
+        content: "즐거운 일본 여행 플랜에 초대되었습니다."
+      },
+      {
+        notificationId: 4,
+        content: "제발 보내줘 기막힌 휴양지 여행 플랜에 초대되었습니다."
+      },
+      {
+        notificationId: 5,
+        content: "troment님이 친구 요청을 보냈습니다."
+      },
+      {
+        notificationId: 5,
+        content: "troment님이 친구 요청을 보냈습니다."
+      },
+    ];
+
+    if (this.notifications.length) {
+      this.notificationClicked = false;
+    } else {
+      this.notificationClicked = true;
+    }
+  },
+  data() {
+    return {
+      notifications: [],
+      notificationClicked: false
+    }
+  },
+  methods: {
+    notificationClick: function () {
+      this.notificationClicked = true;
+
+    }
+  }
 };
 </script>
 
@@ -64,5 +143,93 @@ export default {
 .header-right {
   display: flex;
   align-items: center;
+}
+
+.notification {
+  position: relative;
+  
+  cursor: pointer;
+
+  margin-right: 15px;
+}
+
+.alarmanimation {
+  animation: rotate_image 1.5s linear infinite;
+  transform-origin: 50% 50%;
+}
+
+@keyframes rotate_image{
+    0% {
+      transform: rotate(0deg);
+    }
+    10% {
+      transform: rotate(15deg);
+    }
+    20% {
+      transform: rotate(-15deg);
+    }
+    30%{
+      transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(0deg);
+    }
+}
+
+.notification-dropdown {
+  width: 450px;
+  max-height: 300px;
+  overflow: scroll;
+}
+
+.dropdown .dropdown-menu {
+  --bs-dropdown-padding-y: 0 !important;
+  /* padding-top: 0 !important;
+  padding-bottom: 0 !important; */
+}
+
+.notification-dropdown-divider {
+  width: 100%;
+  border: solid 0.5px gray;
+}
+
+.notification-dropdown-onenoti {
+  width: 100%;
+  height: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.notification-dropdown-onenoti-contents {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 10px;
+}
+
+.notification-dropdown-text {
+  width: 330px;
+}
+
+.notification-dropdown-btns {
+  width: 100px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.reddot {
+  position: absolute;
+
+  background-color: red;
+  width: 9px;
+  height: 9px;
+  border: solid 0px;
+  border-radius: 100%;
+
+  top: 7px;
+  right: 8px;
 }
 </style>
