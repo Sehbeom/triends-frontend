db3aa3848d9c25b4a34d0ea03f9fa8c4f43042cd
Shin Woojong <shinuj9966@gmail.com>
2023-05-22 11:45:15 +0900

    [feat] router link notice, review page

--- triends/src/components/header/PageButtonHeader.vue
+++ triends/src/components/header/PageButtonHeader.vue
@@ -1,37 +1,45 @@
 <template>
   <div class="button-layout">
     <div class="button-container">
-      <page-button-item
-        title="공지사항"
-        link="notice"
-        icon="./icons/notice.png"
-      />
-      <page-button-item
-        title="여행지 추천"
-        link="recommand"
-        icon="./icons/recommand.png"
-      />
-      <page-button-item title="플랜짜기" link="plan" icon="./icons/plan.png" />
-      <page-button-item
-        title="내 플랜"
-        link="myplan"
-        icon="./icons/myplan.png"
-      />
-      <page-button-item
-        title="여행지 리뷰"
-        link="review"
-        icon="./icons/review.png"
-      />
+      <router-link :to="{ name: 'notice' }">
+        <page-button-item title="공지사항" link="notice" :icon="notice" />
+      </router-link>
+      <router-link :to="{ name: 'recommand' }">
+        <page-button-item title="여행지 추천" link="recommand" :icon="recommand" />
+      </router-link>
+      <router-link :to="{ name: 'plan' }">
+        <page-button-item title="플랜짜기" link="plan" :icon="plan" />
+      </router-link>
+      <router-link :to="{ name: 'myplan' }">
+        <page-button-item title="내 플랜" link="myplan" :icon="myplan" />
+      </router-link>
+      <router-link :to="{ name: 'review' }">
+        <page-button-item title="여행지 리뷰" :icon="review" />
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
 import PageButtonItem from "./PageButtonItem.vue";
+import icon1 from "@/assets/icons/notice.png";
+import icon2 from "@/assets/icons/recommand.png";
+import icon3 from "@/assets/icons/plan.png";
+import icon4 from "@/assets/icons/myplan.png";
+import icon5 from "@/assets/icons/review.png";
 
 export default {
   name: "PageButtonHeader",
   components: { PageButtonItem },
+  data() {
+    return {
+      notice: icon1,
+      recommand: icon2,
+      plan: icon3,
+      myplan: icon4,
+      review: icon5,
+    };
+  },
 };
 </script>
 
